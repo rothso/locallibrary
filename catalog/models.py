716895ffc2019b46fb212baeacb7aa95cd54a606
Rothanak So <rothanak.hello@gmail.com>
2018-08-03 12:04:32 -0400

    Add remaining models and make migration

--- catalog/models.py
+++ catalog/models.py
@@ -13,10 +13,29 @@ class Genre(models.Model):
         return self.name
 
 
+class Author(models.Model):
+    """Model representing an author"""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a specific author"""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the author object"""
+        return f'{self.last_name}, {self.first_name}'
+
+
 class Book(models.Model):
     """Model representing a book definition (but not an actual copy of a book)"""
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character ISBN number')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
@@ -32,8 +51,8 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     """Model representing a copy of a book (i.e. that can be borrowed)"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), help_text='Unique in-library ID')
-    book = models.ForeignKey(Book, on_delete=models.SET_NULLA, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique in-library ID')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
