1186966aad84f92514446753b386afa9b938a547
Rothanak So <rothanak.hello@gmail.com>
2018-08-04 17:35:43 -0400

    Display number of genres and dog books on index page

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Author, Book, BookInstance
+from .models import Author, Book, BookInstance, Genre
 
 
 def index(request):
@@ -10,15 +10,21 @@ def index(request):
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # Books about dogs
+    num_dog_books = Book.objects.filter(title__icontains='dog').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_dog_books': num_dog_books,
     }
 
     # Render the HTML template with the data in the context variable
