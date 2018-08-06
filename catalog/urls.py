6f9126112efb41fbc48e5400660aedd33da64af9
Rothanak So <rothanak.hello@gmail.com>
2018-08-06 11:27:02 -0400

    Add permissions for viewing borrowed books

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('books/my', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
+    # Librarian-only paths
+    path('borrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
 ]
